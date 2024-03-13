1f1a294c0dea46ea50175321e39230d6af0a450b
givigelashvili <55489231+aquarazorda@users.noreply.github.com>
2024-03-13 19:37:12 +0400

    chore: autoredirect to admin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
+import { redirect } from "next/navigation";
+
 export default function HomePage() {
+  return redirect("/admin");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       Coming soon...{" "}
