0a41f8627b8f913d8a83337822f1ffc1e7e70938
aquarazorda <g32.gelashvili@gmail.com>
2024-11-07 16:57:43 +0400

    chore: compiler with table no memo

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
   experimental: {
     // ppr: true,
     // dynamicIO: true,
-    // reactCompiler: true,
+    reactCompiler: true,
     serverActions: {
       bodySizeLimit: "5mb",
     },
